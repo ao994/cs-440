bbd6c440285a08f56762f15a64be8dbe2fb03c71
Alyssa Ortiz <ao994@nau.edu>
2024-10-01 23:37:29 -0700

    Update Login.jsx

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -5,9 +5,9 @@ import { useNavigate } from "react-router-dom";
 
 function Login() {    
 
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
-    const navigate = useNavigate()
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault()
